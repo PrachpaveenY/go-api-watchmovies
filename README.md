c330343df153119ab1f3acadf173a4b85c0169f6
Prachpaveen Y <prachpaveen888@gmail.com>
2023-02-17 15:36:02 +0700

    update react

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # go-api-watchmovies
+- Incomplete
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { Fragment } from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
 
 function App() {
-
   return (
+    <BrowserRouter>
     <div className="container">
-    </div>
+
+      <div className="row">
+        <h1 className="mt-3">
+          Go Watch a Movie!
+        </h1>
+        <hr className="mb=3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/admin">Management</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="col-md-10">
+        <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>
+            <Route path="/movies">
+              <Movies/>
+            </Route>
+            <Route path="/admin">
+              <Admin/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>    
+    </BrowserRouter>
   )
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Movies() {
+  return (
+  <h2>Movies</h2>
+  )
+}
+
+function Admin() {
+  return <h2>Management</h2>
 }
 
 export default App
